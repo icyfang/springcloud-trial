450389d07b9ef18c01158c2546091280b95d8732
icyfang <chuche1993@qq.com>
2020-07-27 22:46:29 +0800

    Initial commit

---
+++ src/main/java/com/example/springcloudtrial/SpringcloudTrialApplication.java
@@ -0,0 +1,13 @@
+package com.example.springcloudtrial;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringcloudTrialApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringcloudTrialApplication.class, args);
+    }
+
+}
