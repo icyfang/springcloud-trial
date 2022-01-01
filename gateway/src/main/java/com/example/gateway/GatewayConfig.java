4635a2872947764d418a87b863010e7698c82cf3
icyfang <chuche1993@qq.com>
2022-01-01 23:09:00 +0800

    add gateway module

---
+++ gateway/src/main/java/com/example/gateway/GatewayConfig.java
@@ -0,0 +1,22 @@
+package com.example.gateway;
+
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * @author Hodur
+ * @date 2021-04-28
+ */
+@Configuration
+public class GatewayConfig {
+
+    @Bean
+    public RouteLocator customRouteLocator(RouteLocatorBuilder builder) {
+        return builder.routes()
+                .route("path_route", r -> r.path("/about")
+                        .uri("http://ityouknow.com"))
+                .build();
+    }
+}
