dde0d0a5aaad28c5a002f15f4b674531d0bd4a3b
icyfang <chuche1993@qq.com>
2022-04-18 23:21:24 +0000

    code format

--- feign-consumer/src/main/java/com/example/feignconsumer/upload/UploadService.java
+++ feign-consumer/src/main/java/com/example/feignconsumer/upload/UploadService.java
@@ -1,5 +1,6 @@
 package com.example.feignconsumer.upload;
 
+import com.example.feignconsumer.file.FormSupportConfig;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -7,8 +8,9 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestPart;
 import org.springframework.web.multipart.MultipartFile;
 
-@FeignClient(name = "file", url = "http://localhost:8092", configuration = FeignSupportConfig.class)
+@FeignClient(name = "upload", url = "http://localhost:8092", configuration = FormSupportConfig.class)
 public interface UploadService {
+
     @RequestMapping(value = "/file/upload", consumes = MediaType.MULTIPART_FORM_DATA_VALUE, method = RequestMethod.POST)
     String handleFileUpload(@RequestPart(value = "file") MultipartFile file);
 }
